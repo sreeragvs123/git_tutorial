1f66ccd5ed67ba8f8068acda56b1def0373c2b27
sreerag <sreeragvs1812@gmail.com>
2025-11-02 14:31:24 +0530

    Version 1 restored

---
+++ intro2.java
@@ -0,0 +1,8 @@
+package GitHub;
+
+public class intro2 {
+    public static void main(String args[]){
+        System.out.println("This is the second line");
+    }
+    
+}
--- pratice.java
+++ pratice.java
@@ -3,7 +3,6 @@ package GitHub;
 public class pratice {
     public static void main(String [] args){
         System.out.println("Hi welcome");
-        System.out.println("This is a change");
     }
     
 }
