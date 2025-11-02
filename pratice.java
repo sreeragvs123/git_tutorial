a03e48fb1dcf92e8b962cecc1db0d3505a5cd447
sreerag <sreeragvs1812@gmail.com>
2025-11-02 15:35:46 +0530

    new Version

--- pratice.java
+++ pratice.java
@@ -2,7 +2,7 @@ package GitHub;
 
 public class pratice {
     public static void main(String [] args){
-        System.out.println("Hi welcome");
+        System.out.println("Hi welcome i am a version");
     }
     
 }
